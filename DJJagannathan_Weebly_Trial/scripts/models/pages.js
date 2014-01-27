c9caecfa47c579cb8adef01af8d5f46755a5dee9
DJ Jagannathan <desjag@gmail.com>
2014-01-27 02:04:38 -0500

    just resize and close 2

--- DJJagannathan_Weebly_Trial/scripts/models/pages.js
+++ DJJagannathan_Weebly_Trial/scripts/models/pages.js
@@ -2,8 +2,6 @@
 Weeblys.Page = DS.Model.extend({
 	title: DS.attr('string'),
 	isSelected: DS.attr('boolean'),
-	bodyTitle: DS.hasMany('title'),
-	bodyContent: DS.hasMany('body')
 });
 
 Weeblys.Page.FIXTURES = [
@@ -13,31 +11,5 @@ Weeblys.Page.FIXTURES = [
 		isSelected: true,
 		bodyTitle: 1,
 		bodyContent: 1
-	}
-];
-
-Weeblys.Title = DS.Model.extend({
-	name: DS.attr('string'),
-	page: DS.belongsTo('Page')
-});
-
-Weeblys.Title.FIXTURES = [
-	{
-		id: 1,
-		name: 'Add Title Here',
-		page: 1
-	}
-];
-
-Weeblys.Body = DS.Model.extend({
-	content: DS.attr('string'),
-	page: DS.belongsTo('Page')	
-});
-
-Weeblys.Body.FIXTURES = [
-	{
-		id: 1,
-		content: 'BODY CONTENT HERERERERERERE',
-		page: 1
 	}
 ];
