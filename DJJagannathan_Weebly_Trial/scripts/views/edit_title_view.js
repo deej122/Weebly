49c79610ef424ee5c62afdbd5bcc73757a60b70a
deej122 <djag@umich.edu>
2014-01-28 19:29:38 -0500

    Delete edit_title_view.js

--- DJJagannathan_Weebly_Trial/scripts/views/edit_title_view.js
+++
@@ -1,8 +0,0 @@
-//View for Editing Title Content
-Weeblys.TitleEditView = Ember.TextField.extend({
-	didInsertElement: function () {
-		this.$().focus();
-	}
-});
-
-Ember.Handlebars.helper('edit-title', Weeblys.EditTitleView);
